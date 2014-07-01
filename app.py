b5c92c0cf767b5768c4f5b7fdb35da48b8a3aab9
Dan Brinkman <djbrink@ucf.edu>
2014-07-01 14:11:53 -0400

    moved stuff into config file

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, make_response
-#from config import *
+from config import *
 
 import requests
 import json
@@ -12,10 +12,12 @@ def index():
 
 @app.route('/file/<int:file_id>')
 def get_file(file_id):
-	url = "https://webcourses2c.test.instructure.com/api/v1/files/%s?access_token=blah" % file_id
+
+	url ="%sfiles/%s?access_token=%s" % (API_URL, file_id, API_KEY)
+	
 	r = requests.get(url)
 	data = r.json()
-	print data
+	
 	imageRequest = requests.get(data["url"])
 
 	resp = make_response(imageRequest.content)
