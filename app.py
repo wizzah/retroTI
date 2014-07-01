42a24f05e03e4438f36f62c8288eef46cf4b06a9
Dan Brinkman <djbrink@ucf.edu>
2014-07-01 13:58:11 -0400

    added file getting route

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-from flask import Flask, render_template
-from config import *
+from flask import Flask, make_response
+#from config import *
+
+import requests
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 	return "Emulator LTI"
+
+@app.route('/file/<int:file_id>')
+def get_file(file_id):
+	url = "https://webcourses2c.test.instructure.com/api/v1/files/%s?access_token=13~0hAto3fEbmP1tKSKbU7ivAyQpD5DpYwNAsrAUsFQiuTwl9nB9fnwUrw1gY3YVtaQ" % file_id
+	r = requests.get(url)
+	data = r.json()
+	print data
+	imageRequest = requests.get(data["url"])
+
+	resp = make_response(imageRequest.content)
+	resp.headers['Content-Type'] = data["content-type"]
+
+	return resp
+
 
 if __name__ == '__main__':
 	app.run()
